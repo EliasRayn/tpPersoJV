1c0f5c61e1c0de5c172003b554656db344d7ed9f
R-Elias <114922778+R-Elias@users.noreply.github.com>
2023-11-09 17:54:01 +0100

    Add tailwind, button, card

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import { Card } from './design/Card'
 import './App.css'
+import { Button } from './design/Button'
+import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  return (
-    <Card>
-      <p> Hello </p>
-    </Card>
+  const toggleLogin = () => {
+    setIsLoggedIn(!isLoggedIn)
+  }
+
+  const loginPage = (
+    <div className="flex justify-center items-center bg-black" style={{width: '100vw', height: '100vh', margin: '0px', padding: '0px'}}>
+      <Card>
+        <div className="flex justify-center items-center p-10">
+          <img src="../public/bowser.webp"></img>
+        </div>
+        <p style={{ color: "white" }}>Hey, pas si vite ! Vous n'êtes pas connecté</p>
+        <br/>
+        <div className="flex p-0 m-0 justify-center items-center">
+          <div onClick={toggleLogin} className="w-fit">
+            <Button> Se connecter </Button>
+          </div>
+        </div>
+        <br/>
+        <br/>
+      </Card>
+    </div>
   )
+
+  const homePage = (
+    <div className="flex justify-center items-center bg-black" style={{width: '100vw', height: '100vh', margin: '0px', padding: '0px'}}>
+      <Card>
+      <div className="grid grid-rows-3 grid-cols-4 gap-4">
+      <div className="gridCell row-span-1 col-span-1">01</div>
+      <div className="gridCell row-span-1 col-span-1">02</div>
+      <div className="gridCell row-span-2 col-span-2">03</div>
+      <div className="gridCell row-span-1 col-span-2">04</div>
+      <div className="gridCell row-span-1 col-span-1">06</div>
+      <div className="gridCell row-span-1 col-span-1">07</div>
+      <div className="gridCell row-span-1 col-span-1">08</div>
+      <div className="gridCell row-span-1 col-span-1">09</div>
+    </div>
+        <div className="flex p-0 m-0 justify-center items-center">
+          <div onClick={toggleLogin} className="w-fit">
+            <Button> Se déconnecter </Button>
+          </div>
+        </div>
+      </Card>
+    </div>
+  )
+
+  return isLoggedIn ? homePage : loginPage
 }
 
 export default App
--- src/design/Card.tsx
+++ src/design/Card.tsx
@@ -1,7 +1,7 @@
 import { PropsWithChildren } from "react";
 
 export function Card({children}:PropsWithChildren){
-  return <div style={{backgroundColor:"darkgray",borderRadius:"12px",minHeight:40, padding: "10px"}}>
+  return <div style={{backgroundColor:"#383838",borderRadius:"12px",minHeight:40, padding: "10px"}}>
  {children}
   </div>
 }
